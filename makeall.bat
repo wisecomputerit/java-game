9ad9bb89d646190062202202dd74332b097847db
Wise Courses Ltd. <wise.computer.it@gmail.com>
2024-10-13 14:03:19 +0800

    Add files via upload

---
+++ makeall.bat
@@ -0,0 +1,47 @@
+cd c1
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c2
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c3
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c4
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c5
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c6
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c7
+del *.class
+del *.jar
+call make
+cd ..
+
+cd c8
+del *.class
+del *.jar
+call make
+cd ..
